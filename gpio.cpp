6a525c7f792da7ea64416c0379320b2eeb95717f
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2024-10-14 11:31:54 -0300

    :sparkles: added new code to main

--- gpio.cpp
+++ gpio.cpp
@@ -1,5 +1,34 @@
 #include "gpio.h"
 
-void func() {
-    
+
+GPIO_Pin::GPIO_Pin(GPIO_Port_Name port, int pin, GPIO_Direction dir){
+
+    // cria o valor 1 (0000 0001) e  realiza um shift para o número pin;
+    mask = 1 << pin;
+
+    port = (GPIO_Port* ) PORT_ADDR[port_name];
+
+    if(dir == OUTPUT){
+        port->ddr |= mask;
+    }else{
+        port->ddr &= ~mask;
+    }
+
+}
+
+void GPIO_Pin::set(){
+
+}
+
+void GPIO_Pin::clear(){
+
+}
+
+int GPIO_Pin::read(){
+
+}
+
+int GPIO_Pin::write(){
+
+
 }
--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
+#include <avr/io.h>
 #include "gpio.h"
+
+const int LED1_PIN = 5;    // Porta B 5
+const int LED2_PIN = 2;    // Porta B 2
+const int BUTTON_PIN = 8;  // Porta B 0
+
 
 void setup()
 {
